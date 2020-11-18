c74409c775d199fd990514d41bac916e382c62c9
Marco Catalán <mcat95@gmail.com>
2020-11-18 14:00:20 +0100

    Add form to generate JSON

--- public/js/doc.js
+++
@@ -1,6 +0,0 @@
-const init = async () => {
-  const markdown = await fetch('md/jsonlead_v2.md').then(r => r.text())
-  document.getElementById('content').innerHTML = marked(markdown);
-}
-
-document.addEventListener('DOMContentLoaded', init);
