6a6a27517d46012b460198a5a04bf52293958d2e
Guzmán <guzlosan@gmail.com>
2021-03-10 12:22:12 +0100

    Revert "Make Preview window fixed"
    This reverts commit 2c594c65913dad21698fcdb705f6cd4316a76937.

--- public/js/form.js
+++ public/js/form.js
@@ -34,18 +34,10 @@ const removeEmpty = obj => Object
 const Preview = ({ data }) => {
   const ref = useRef();
   console.log(data);
-  return e('pre', {
-    style: {
-      position: 'fixed',
-      width: '47.5%'
-    },
-    dangerouslySetInnerHTML: {
-      __html: prettyPrintJson.toHtml(removeEmpty(data), {
-        indent: 2,
-        quoteKeys: true,
-      })
-    }
-  });
+  return e('pre', {dangerouslySetInnerHTML: {__html: prettyPrintJson.toHtml(removeEmpty(data), {
+    indent: 2,
+    quoteKeys: true,
+  })}});
 }
 
 
