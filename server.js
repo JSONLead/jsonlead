393db51dbf4044697b747c0c098500c32b04e0f1
Marco Catalán <mcat95@gmail.com>
2022-01-12 10:32:26 +0100

    Add cors support to jsonlead server

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const marked  = require('marked');
 const path    = require('path');
 const fs      = require('fs');
+const cors    = require('cors')
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.static('public'));
 
 marked.use({ renderer: {
