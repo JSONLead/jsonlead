84660f3e3c5fb0c3c5f996d8b807e79d5f8c66a0
Marco Catalán <mcat95@gmail.com>
2024-03-05 13:36:03 +0100

    Add version selector and rename schema versions

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ import CompressibleObjectFieldTemplate from './CompressibleObjectFieldTemplate.j
 import Form                            from '@rjsf/core';
 import JsonView                        from '@uiw/react-json-view';
 
+const VERSIONS = {
+  '1.0.0': { default: { version: '1.0.0', client: {} }},
+  '2.0.0': { default: { }},
+}
+const DEFAULT_VERSION = '2.0.0';
+
 const removeEmpty = obj => Object
   .keys(obj)
   .map(key => {
@@ -33,14 +39,22 @@ const removeEmpty = obj => Object
 const App = () => {
   const [ schema, setSchema ] = useState();
   const [ data, setData ] = useState({});
+  const [ version, setVersion ] = useState(DEFAULT_VERSION);
   useEffect(() => {
-    fetch('schemas/jsonlead_v3.schema.json')
+    fetch(`schemas/jsonlead_v${version}.schema.json`)
       .then(r => r.json())
-      .then(setSchema)
-  }, []); 
+      .then(s => {
+        setData(VERSIONS[version].default);
+        setSchema(s);
+      })
+  }, [ version ]); 
   return schema ? (
     <div style={{display: 'flex', width: '100%', height: '100vh'}}>
-      <div style={{flex: 1, padding: '2em', overflow: 'auto'}}>
+      <div style={{flex: 1, display: 'flex', flexDirection: 'column', gap: '1em', padding: '2em', overflow: 'auto'}}>
+        <select className='form-control' value={version} onChange={e => setVersion(e.target.value)}>
+          <option value="1.0.0">Version 1.0.0</option>
+          <option value="2.0.0">Version 2.0.0</option>
+        </select>
         <Form
           schema={schema}
           validator={validator}
@@ -51,6 +65,7 @@ const App = () => {
               'company_information': {'ui:compressible': true},
               'address': {'ui:compressible': true},
             },
+            'client': { 'ui:compressible': true },
             'sales_lead': { 'ui:compressible': true },
             'appraisal_lead': { 'ui:compressible': true },
             'aftersales_lead': { 'ui:compressible': true },
