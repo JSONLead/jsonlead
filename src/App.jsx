461aee9e1ae4d8ce92486faaecf5a2302559afa0
Marco Catalán <mcat95@gmail.com>
2024-03-05 13:19:54 +0100

    Add new builder for v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,70 @@
-function App() {
+import {
+  useState,
+  useEffect,
+}                                      from 'react';
+import validator                       from '@rjsf/validator-ajv8';
+import CompressibleObjectFieldTemplate from './CompressibleObjectFieldTemplate.jsx';
+import Form                            from '@rjsf/core';
+import JsonView                        from '@uiw/react-json-view';
 
-  return (
-    <div>
-      asd
+const removeEmpty = obj => Object
+  .keys(obj)
+  .map(key => {
+    const v = obj[key];
+    if(typeof v === 'object' && !Array.isArray(v) && Object(v) === v) {
+      if(Object.keys(v).length > 0){
+        const emptied = removeEmpty(v);
+        if(Object.keys(emptied).length > 0){
+          return {[key]: emptied};
+        } else {
+          return null;
+        }
+      } else {
+        return null;
+      }
+    } else if (v != null && !(Array.isArray(v) && !v.length)) {
+      return {[key]: v};
+    } else return null;
+
+  })
+  .filter(f => f)
+  .reduce((p,c) => Object.assign({}, p, c),{});
+
+const App = () => {
+  const [ schema, setSchema ] = useState();
+  const [ data, setData ] = useState({});
+  useEffect(() => {
+    fetch('schemas/jsonlead_v3.schema.json')
+      .then(r => r.json())
+      .then(setSchema)
+  }, []); 
+  return schema ? (
+    <div style={{display: 'flex', width: '100%', height: '100vh'}}>
+      <div style={{flex: 1, padding: '2em', overflow: 'auto'}}>
+        <Form
+          schema={schema}
+          validator={validator}
+          formData={data}
+          uiSchema={{
+            'customer': {
+              'ui:compressible': true,
+              'company_information': {'ui:compressible': true},
+              'address': {'ui:compressible': true},
+            },
+            'sales_lead': { 'ui:compressible': true },
+            'appraisal_lead': { 'ui:compressible': true },
+            'aftersales_lead': { 'ui:compressible': true },
+          }}
+          liveValidate
+          onChange={e => setData(e.formData)}
+          templates={{ ObjectFieldTemplate: CompressibleObjectFieldTemplate }}
+        />
+      </div>
+      <div style={{flex: 1, padding: '2em', borderLeft: '1px solid black'}}>
+        <JsonView value={removeEmpty(data)} displayDataTypes={false} shortenTextAfterLength={Infinity}/>
+      </div>
     </div>
-  )
+  ) : 'Loading schema...';
 }
 
 export default App
---
+++ src/CompressibleObjectFieldTemplate.jsx
@@ -0,0 +1,98 @@
+import { useState }        from 'react';
+import ObjectFieldTemplate from '@rjsf/core/lib/components/templates/ObjectFieldTemplate';
+import {
+  canExpand,
+  descriptionId,
+  getTemplate,
+  getUiOptions,
+  titleId,
+}                          from '@rjsf/utils';
+
+
+const TitleFieldTemplate = ({ id, title, required, compressed, setCompressed }) => {
+  return (
+    <legend id={id}>
+      {title}
+      {required && <span className='required'>*</span>}
+      {setCompressed && <span
+        style={{
+          marginLeft: '1em',
+          backgroundColor: 'gray',
+          borderRadius: '50%',
+          width: '1em',
+          height: '1em',
+          display: 'inline-flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          cursor: 'pointer',
+          userSelect: 'none',
+        }}
+        onClick={() => setCompressed(d => !d)}
+      >
+        {compressed ? '+' : '-'}
+      </span>}
+    </legend>
+  );
+}
+
+const CompressibleObjectFieldTemplate = props => {
+  const {
+    description,
+    disabled,
+    formData,
+    idSchema,
+    onAddClick,
+    properties,
+    readonly,
+    registry,
+    required,
+    schema,
+    title,
+    uiSchema,
+  } = props;
+  const options = getUiOptions(uiSchema);
+  const [ compressed, setCompressed ] = useState(options.compressible);
+  const DescriptionFieldTemplate = getTemplate('DescriptionFieldTemplate', registry, options);
+  // Button templates are not overridden in the uiSchema
+  const {
+    ButtonTemplates: { AddButton },
+  } = registry.templates;
+  return (
+    <fieldset id={idSchema.$id}>
+      {title && (
+        <TitleFieldTemplate
+          id={titleId(idSchema)}
+          title={title}
+          required={required}
+          compressed={compressed}
+          setCompressed={options.compressible && setCompressed}
+        />
+      )}
+      {!compressed && description && (
+        <DescriptionFieldTemplate
+          id={descriptionId(idSchema)}
+          description={description}
+          schema={schema}
+          uiSchema={uiSchema}
+          registry={registry}
+        />
+      )}
+      {compressed ? null : (
+        <div style={{borderLeft: '1px solid black', paddingLeft: '1em'}}>
+          {properties.map((prop) => prop.content)}
+          {canExpand(schema, uiSchema, formData) && (
+            <AddButton
+              className='object-property-expand'
+              onClick={onAddClick(schema)}
+              disabled={disabled || readonly}
+              uiSchema={uiSchema}
+              registry={registry}
+            />
+          )}
+        </div>
+      )}
+    </fieldset>
+  );
+}
+
+export default CompressibleObjectFieldTemplate;
